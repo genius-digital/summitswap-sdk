dee55d233efcda9179fbee9c128d527c00a4723d
Luka Samkharadze <lukesamkharadze@gmail.com>
2021-10-22 19:03:22 +0400

    fix(contract-addresse): add testnet contract addresses

--- src/constants.ts
+++ src/constants.ts
@@ -19,9 +19,25 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x27aD238316128DC85b23b0EC1eb3f7153afd6860'
+console.log(process.env.REACT_APP_NODE_ENV)
 
-export const INIT_CODE_HASH = '0xfe604490e88884eee0a2ef7362417e3771d067dc74cd2e605d0b3e1e90f300cb'
+export const FACTORY_ADDRESSES = {
+  '56': '0x27aD238316128DC85b23b0EC1eb3f7153afd6860',
+  '97': '0x037768f79c2985079c0B29C0b39F5bf1D598Db83'
+} as {
+  [key: string]: string
+}
+
+export const FACTORY_ADDRESS = FACTORY_ADDRESSES[process.env.REACT_APP_CHAIN_ID as string]
+
+export const INIT_CODE_HASHES = {
+  '56': '0xfe604490e88884eee0a2ef7362417e3771d067dc74cd2e605d0b3e1e90f300cb',
+  '97': '0x3a37cf8ecde8166faa9daa2f4a070be1e622988f207a874db2907fd5220573f1'
+} as {
+  [key: string]: string
+}
+
+export const INIT_CODE_HASH = INIT_CODE_HASHES[process.env.REACT_APP_CHAIN_ID as string]
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
