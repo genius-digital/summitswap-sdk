1fcaf4867e6954977d6be18fcb1e3fe4f21fc79f
erwin-huang <erwinhuank@gmail.com>
2022-03-16 15:40:27 +0700

    add allowSellMax

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -13,6 +13,7 @@ export class Token extends Currency {
   public readonly referralEnabled: boolean
   public readonly buySlippageTolerance: number
   public readonly sellSlippageTolerance: number
+  public readonly allowSellMax: boolean
 
   public constructor(
     chainId: ChainId,
@@ -23,6 +24,7 @@ export class Token extends Currency {
     priority = 0,
     referralEnabled = false,
     buySlippageTolerance = 0,
+    allowSellMax = true,
     sellSlippageTolerance = 0
   ) {
     super(decimals, symbol, name)
@@ -31,6 +33,7 @@ export class Token extends Currency {
     this.referralEnabled = referralEnabled
     this.buySlippageTolerance = buySlippageTolerance
     this.sellSlippageTolerance = sellSlippageTolerance
+    this.allowSellMax = allowSellMax
     this.address = validateAndParseAddress(address)
   }
 
