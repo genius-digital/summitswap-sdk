a5da92ed9fc306576476ba6a1d8f3af52f903d84
lukesamkharadze <lukesamkharadze@gmail.com>
2022-07-14 13:02:38 +0200

    feat(tokens): add other chain weths addresses

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -15,7 +15,7 @@ export class Token extends Currency {
   public readonly sellSlippageTolerance: number
   public readonly coingeckoId?: string
   public readonly allowSellMax: boolean
-  
+
   public constructor(
     chainId: ChainId,
     address: string,
@@ -80,19 +80,187 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
   }
 }
 
-export const WETH = {
-  [ChainId.MAINNET]: new Token(
-    ChainId.MAINNET,
-    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+type AddressMap = { [chainId: number]: string }
+type TokenMap = { [chainId: number]: Token }
+
+export const WETH9_ADDRESS: AddressMap = {
+  [ChainId.ETHEREUM]: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  [ChainId.ROPSTEN]: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+  [ChainId.RINKEBY]: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+  [ChainId.GÖRLI]: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+  [ChainId.KOVAN]: '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
+  [ChainId.ARBITRUM]: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+  [ChainId.ARBITRUM_TESTNET]: '0xf8456e5e6A225C2C1D74D8C9a4cB2B1d5dc1153b',
+  [ChainId.BSC]: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
+  [ChainId.FANTOM]: '0x74b23882a30290451A17c44f4F05243b6b58C76d',
+  [ChainId.MATIC]: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+  [ChainId.MATIC_TESTNET]: '0x714550C2C1Ea08688607D86ed8EeF4f5E4F22323',
+  [ChainId.OKEX]: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
+  [ChainId.HECO]: '0x64FF637fB478863B7468bc97D30a5bF3A428a1fD',
+  [ChainId.HARMONY]: '0x6983D1E6DEf3690C4d616b13597A09e6193EA013',
+  [ChainId.XDAI]: '0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1',
+  [ChainId.AVALANCHE]: '0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB',
+  [ChainId.PALM]: '0x726138359C17F1E56bA8c4F737a7CAf724F6010b',
+  [ChainId.CELO]: '0x122013fd7dF1C6F636a5bb8f03108E876548b455',
+  [ChainId.MOONRIVER]: '0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C',
+  [ChainId.TELOS]: '0xfA9343C3897324496A05fC75abeD6bAC29f8A40f',
+  [ChainId.FUSE]: '0xa722c13135930332Eb3d749B2F0906559D2C5b99',
+  [ChainId.MOONBEAM]: '0x30D2a9F5FDf90ACe8c17952cbb4eE48a55D916A7',
+  [ChainId.OPTIMISM]: '0x4200000000000000000000000000000000000006'
+}
+
+export const WNATIVE_ADDRESS: AddressMap = {
+  [ChainId.ETHEREUM]: WETH9_ADDRESS[ChainId.ETHEREUM],
+  [ChainId.ROPSTEN]: WETH9_ADDRESS[ChainId.ROPSTEN],
+  [ChainId.RINKEBY]: WETH9_ADDRESS[ChainId.RINKEBY],
+  [ChainId.GÖRLI]: WETH9_ADDRESS[ChainId.GÖRLI],
+  [ChainId.KOVAN]: WETH9_ADDRESS[ChainId.KOVAN],
+  [ChainId.OPTIMISM]: WETH9_ADDRESS[ChainId.OPTIMISM],
+  [ChainId.ARBITRUM]: WETH9_ADDRESS[ChainId.ARBITRUM],
+  [ChainId.ARBITRUM_TESTNET]: WETH9_ADDRESS[ChainId.ARBITRUM_TESTNET],
+  [ChainId.FANTOM]: '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
+  [ChainId.FANTOM_TESTNET]: '0xf1277d1Ed8AD466beddF92ef448A132661956621',
+  [ChainId.MATIC]: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+  [ChainId.MATIC_TESTNET]: '0x5B67676a984807a212b1c59eBFc9B3568a474F0a',
+  [ChainId.XDAI]: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d',
+  [ChainId.BSC]: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+  [ChainId.BSC_TESTNET]: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+  [ChainId.MOONBEAM_TESTNET]: '0x372d0695E75563D9180F8CE31c9924D7e8aaac47',
+  [ChainId.AVALANCHE]: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+  [ChainId.AVALANCHE_TESTNET]: '0xd00ae08403B9bbb9124bB305C09058E32C39A48c',
+  [ChainId.HECO]: '0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F',
+  [ChainId.HECO_TESTNET]: '0x5B2DA6F42CA09C77D577a12BeaD0446148830687',
+  [ChainId.HARMONY]: '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a',
+  [ChainId.HARMONY_TESTNET]: '0x7a2afac38517d512E55C0bCe3b6805c10a04D60F',
+  [ChainId.OKEX]: '0x8F8526dbfd6E38E3D8307702cA8469Bae6C56C15',
+  [ChainId.OKEX_TESTNET]: '0x2219845942d28716c0F7C605765fABDcA1a7d9E0',
+  [ChainId.PALM]: '0xF98cABF0a963452C5536330408B2590567611a71',
+  [ChainId.CELO]: '0x471EcE3750Da237f93B8E339c536989b8978a438',
+  [ChainId.MOONRIVER]: '0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d',
+  [ChainId.FUSE]: '0x0BE9e53fd7EDaC9F859882AfdDa116645287C629',
+  [ChainId.TELOS]: '0xD102cE6A4dB07D247fcc28F366A623Df0938CA9E',
+  [ChainId.MOONBEAM]: '0xAcc15dC74880C9944775448304B263D191c6077F'
+}
+
+export const WETH9: TokenMap = {
+  [ChainId.ETHEREUM]: new Token(ChainId.ETHEREUM, WETH9_ADDRESS[ChainId.ETHEREUM], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.ROPSTEN]: new Token(ChainId.ROPSTEN, WETH9_ADDRESS[ChainId.ROPSTEN], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, WETH9_ADDRESS[ChainId.RINKEBY], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, WETH9_ADDRESS[ChainId.GÖRLI], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, WETH9_ADDRESS[ChainId.KOVAN], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.ARBITRUM]: new Token(ChainId.ARBITRUM, WETH9_ADDRESS[ChainId.ARBITRUM], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.ARBITRUM_TESTNET]: new Token(
+    ChainId.ARBITRUM_TESTNET,
+    WETH9_ADDRESS[ChainId.ARBITRUM_TESTNET],
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.BSC]: new Token(ChainId.BSC, WETH9_ADDRESS[ChainId.BSC], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.FANTOM]: new Token(ChainId.FANTOM, WETH9_ADDRESS[ChainId.FANTOM], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, WETH9_ADDRESS[ChainId.MATIC], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.MATIC_TESTNET]: new Token(
+    ChainId.MATIC_TESTNET,
+    WETH9_ADDRESS[ChainId.MATIC_TESTNET],
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.OKEX]: new Token(ChainId.OKEX, WETH9_ADDRESS[ChainId.OKEX], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.HECO]: new Token(ChainId.HECO, WETH9_ADDRESS[ChainId.HECO], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.HARMONY]: new Token(ChainId.HARMONY, WETH9_ADDRESS[ChainId.HARMONY], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.XDAI]: new Token(ChainId.XDAI, WETH9_ADDRESS[ChainId.XDAI], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, WETH9_ADDRESS[ChainId.AVALANCHE], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.PALM]: new Token(ChainId.PALM, WETH9_ADDRESS[ChainId.PALM], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.TELOS]: new Token(ChainId.TELOS, WETH9_ADDRESS[ChainId.TELOS], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.FUSE]: new Token(ChainId.FUSE, WETH9_ADDRESS[ChainId.FUSE], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.MOONBEAM]: new Token(ChainId.MOONBEAM, WETH9_ADDRESS[ChainId.MOONBEAM], 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.OPTIMISM]: new Token(ChainId.OPTIMISM, WETH9_ADDRESS[ChainId.OPTIMISM], 18, 'WETH', 'Wrapped Ether')
+}
+
+export const WETH: TokenMap = {
+  [ChainId.ETHEREUM]: WETH9[ChainId.ETHEREUM],
+  [ChainId.ROPSTEN]: WETH9[ChainId.ROPSTEN],
+  [ChainId.RINKEBY]: WETH9[ChainId.RINKEBY],
+  [ChainId.GÖRLI]: WETH9[ChainId.GÖRLI],
+  [ChainId.KOVAN]: WETH9[ChainId.KOVAN],
+  [ChainId.OPTIMISM]: WETH9[ChainId.OPTIMISM],
+  [ChainId.FANTOM]: new Token(ChainId.FANTOM, WNATIVE_ADDRESS[ChainId.FANTOM], 18, 'WFTM', 'Wrapped FTM'),
+  [ChainId.FANTOM_TESTNET]: new Token(
+    ChainId.FANTOM_TESTNET,
+    WNATIVE_ADDRESS[ChainId.FANTOM_TESTNET],
+    18,
+    'FTM',
+    'Wrapped FTM'
+  ),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, WNATIVE_ADDRESS[ChainId.MATIC], 18, 'WMATIC', 'Wrapped Matic'),
+  [ChainId.MATIC_TESTNET]: new Token(
+    ChainId.MATIC_TESTNET,
+    WNATIVE_ADDRESS[ChainId.MATIC_TESTNET],
+    18,
+    'WMATIC',
+    'Wrapped Matic'
+  ),
+  [ChainId.XDAI]: new Token(ChainId.XDAI, WNATIVE_ADDRESS[ChainId.XDAI], 18, 'WXDAI', 'Wrapped xDai'),
+  [ChainId.BSC]: new Token(ChainId.BSC, WNATIVE_ADDRESS[ChainId.BSC], 18, 'WBNB', 'Wrapped BNB'),
+  [ChainId.BSC_TESTNET]: new Token(
+    ChainId.BSC_TESTNET,
+    WNATIVE_ADDRESS[ChainId.BSC_TESTNET],
     18,
     'WBNB',
     'Wrapped BNB'
   ),
-  [ChainId.BSCTESTNET]: new Token(
-    ChainId.BSCTESTNET,
-    '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+  [ChainId.ARBITRUM]: WETH9[ChainId.ARBITRUM],
+  [ChainId.ARBITRUM_TESTNET]: WETH9[ChainId.ARBITRUM_TESTNET],
+  [ChainId.MOONBEAM_TESTNET]: new Token(
+    ChainId.MOONBEAM_TESTNET,
+    WNATIVE_ADDRESS[ChainId.MOONBEAM_TESTNET],
     18,
-    'WBNB',
-    'Wrapped BNB'
-  )
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, WNATIVE_ADDRESS[ChainId.AVALANCHE], 18, 'WAVAX', 'Wrapped AVAX'),
+  [ChainId.AVALANCHE_TESTNET]: new Token(
+    ChainId.AVALANCHE_TESTNET,
+    WNATIVE_ADDRESS[ChainId.AVALANCHE_TESTNET],
+    18,
+    'WAVAX',
+    'Wrapped AVAX'
+  ),
+  [ChainId.HECO]: new Token(ChainId.HECO, WNATIVE_ADDRESS[ChainId.HECO], 18, 'WHT', 'Wrapped HT'),
+  [ChainId.HECO_TESTNET]: new Token(
+    ChainId.HECO_TESTNET,
+    WNATIVE_ADDRESS[ChainId.HECO_TESTNET],
+    18,
+    'WHT',
+    'Wrapped HT'
+  ),
+  [ChainId.HARMONY]: new Token(ChainId.HARMONY, WNATIVE_ADDRESS[ChainId.HARMONY], 18, 'WONE', 'Wrapped ONE'),
+  [ChainId.HARMONY_TESTNET]: new Token(
+    ChainId.HARMONY_TESTNET,
+    WNATIVE_ADDRESS[ChainId.HARMONY_TESTNET],
+    18,
+    'WONE',
+    'Wrapped ONE'
+  ),
+  [ChainId.OKEX]: new Token(ChainId.OKEX, WNATIVE_ADDRESS[ChainId.OKEX], 18, 'WOKT', 'Wrapped OKExChain'),
+  [ChainId.OKEX_TESTNET]: new Token(
+    ChainId.OKEX_TESTNET,
+    WNATIVE_ADDRESS[ChainId.OKEX_TESTNET],
+    18,
+    'WOKT',
+    'Wrapped OKExChain'
+  ),
+  [ChainId.CELO]: new Token(ChainId.CELO, WNATIVE_ADDRESS[ChainId.CELO], 18, 'CELO', 'Celo'),
+  [ChainId.PALM]: new Token(ChainId.PALM, WNATIVE_ADDRESS[ChainId.PALM], 18, 'WPALM', 'Wrapped Palm'),
+  [ChainId.MOONRIVER]: new Token(
+    ChainId.MOONRIVER,
+    WNATIVE_ADDRESS[ChainId.MOONRIVER],
+    18,
+    'WMOVR',
+    'Wrapped Moonriver'
+  ),
+  [ChainId.FUSE]: new Token(ChainId.FUSE, WNATIVE_ADDRESS[ChainId.FUSE], 18, 'WFUSE', 'Wrapped Fuse'),
+  [ChainId.TELOS]: new Token(ChainId.TELOS, WNATIVE_ADDRESS[ChainId.TELOS], 18, 'WTLOS', 'Wrapped Telos'),
+  [ChainId.MOONBEAM]: new Token(ChainId.MOONBEAM, WNATIVE_ADDRESS[ChainId.MOONBEAM], 18, 'WGLMR', 'Wrapped Glimmer'),
 }
